999e4fb4d255230eddccc3c94cf638603c822114
huiyan1996 <35520637+huiyan1996@users.noreply.github.com>
2021-07-05 13:26:29 +0800

    add document

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
     $("#sideBar").load("sidebar.html");
+
+    var isLogin = sessionStorage.getItem("isLoginTtwo");
+    if(isLogin) {
+        $("#passwordBlock").hide();
+    }
+});
+
+$(document).on("click", "#submit", function(){
+    var baseString = 'dDJ3ZUIxOTJteQ==';
+    var enteredPwd = $("#password").val();
+    var decodedString = window.btoa(enteredPwd);
+
+    if(decodedString == baseString) {
+        sessionStorage.setItem("isLoginTtwo", true);
+        $("#passwordBlock").hide();
+    }
 });
